51a12b9a482d2d498e8b3745ee4e18991f4f7b07
fesswood <fesswood@kemsu.ru>
2015-06-03 14:32:08 +0700

    before merging

--- app/src/main/java/info/goodline/utubewatcher/VideoList/VideoListActivity.java
+++ app/src/main/java/info/goodline/utubewatcher/VideoList/VideoListActivity.java
@@ -16,6 +16,8 @@ public class VideoListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_list);
+
+
     }
 
 }
